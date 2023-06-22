0ed08e9f9ba87b41b0da8009017ded81631653e0
Cheese <adhesivecheese@gmail.com>
2023-06-22 16:18:30 -0400

    Update init_postgres.sh

--- docker/postgres/init_postgres.sh
+++ docker/postgres/init_postgres.sh
@@ -6,20 +6,17 @@ set -e
 # from reddit app's setup_postgres.sh
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 create or replace function hot(ups integer, downs integer, date timestamp with time zone) returns numeric as \$\$
-    select round(cast(log(greatest(abs((\$1 * 2) + \$2), 1)) * sign((\$1 * 2) + \$2) + (date_part('epoch', \$3) - 1134028003) / 45000.0 as numeric), 7)
-\$\$ language sql immutable;
-
-create or replace function upvotes(ups integer) returns integer as \$\$
-    select \$1
+    select round(cast(log(greatest(abs(\$1 - \$2), 1)) * sign(\$1 - \$2) + (date_part('epoch', \$3) - 1134028003) / 45000.0 as numeric), 7)
 \$\$ language sql immutable;
 
 create or replace function score(ups integer, downs integer) returns integer as \$\$
-    select (\$1 * 2) + \$2
+    select \$1 - \$2
 \$\$ language sql immutable;
 
 create or replace function controversy(ups integer, downs integer) returns float as \$\$
-    select CASE WHEN \$1 < 0 or \$2 <= 0 THEN 0
-                ELSE \$2 + (cast(\$1 as float) / (\$1 + \$2))
+    select CASE WHEN \$1 <= 0 or \$2 <= 0 THEN 0
+                WHEN \$1 > \$2 THEN power(\$1 + \$2, cast(\$2 as float) / \$1)
+                ELSE power(\$1 + \$2, cast(\$1 as float) / \$2)
            END;
 \$\$ language sql immutable;
 
